290265e9af6d60e0783f941441e4bf4c8774423f
foo <foo@example.com>
2023-05-19 02:48:32 +0000

    get title, id, description, date, slug

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -1,7 +1,8 @@
-import { Client } from "@notionhq/client";
+import {Client} from "@notionhq/client";
+import {PageObjectResponse, PartialPageObjectResponse} from "@notionhq/client/build/src/api-endpoints";
 
 const notion = new Client({
-    auth: process.env.NOTION_TOKEN,
+  auth: process.env.NOTION_TOKEN,
 });
 
 export const getAllPosts = async () => {
@@ -10,5 +11,17 @@ export const getAllPosts = async () => {
     page_size: 100,
   });
   const allPosts = posts.results;
-  return allPosts;
+  return allPosts.map((post) => {
+    return getPageMetaData(post);
+  });
+};
+
+const getPageMetaData = (post) => {
+  return {
+    id: post.id,
+    title: post.properties.Name.title[0].plain_text,
+    description: post.properties.Description.rich_text[0].plain_text,
+    date: post.properties.Date.date.start,
+    slug: post.properties.Slug.rich_text[0].plain_text,
+  };
 };
