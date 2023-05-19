d11e42d2ccdbdc5427ee62d8171e06581d6bdbc9
foo <foo@example.com>
2023-05-19 03:05:53 +0000

    get metadata tag

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -17,11 +17,18 @@ export const getAllPosts = async () => {
 };
 
 const getPageMetaData = (post) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+    return allTags;
+  };
   return {
     id: post.id,
     title: post.properties.Name.title[0].plain_text,
     description: post.properties.Description.rich_text[0].plain_text,
     date: post.properties.Date.date.start,
     slug: post.properties.Slug.rich_text[0].plain_text,
+    tags: getTags(post.properties.Tag.multi_select),
   };
 };
