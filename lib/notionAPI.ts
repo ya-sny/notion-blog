6afdff918eccbe09573d489112baf23a52547b8a
foo <foo@example.com>
2023-05-19 02:18:38 +0000

    ssg, getAllPosts

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -8,7 +8,7 @@ export const getAllPosts = async () => {
   const posts = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID,
     page_size: 100,
-  })
+  });
   const allPosts = posts.results;
   return allPosts;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { getAllPosts } from '../lib/notionAPI';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const allPosts = await getAllPosts();
+  return {
+    props: {
+      allPosts: allPosts,
+    },
+    revalidate: 60,
+  }
+}
+
+export default function Home({ allPosts }) {
+  console.log(allPosts);
   return (
     <div>
       <Head>
